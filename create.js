6d600e0162e33e7a9e2d5c30909a67fdd17c99fe
Muhammad Kashaf <kashafahmed72@gmail.com>
2022-01-29 15:19:31 +0500

    appsync crud user

--- create.js
+++ create.js
@@ -4,20 +4,150 @@ const AWS = require("aws-sdk");
 const client = new AWS.DynamoDB.DocumentClient();
 
 module.exports.run = async function (event) {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: "users",
-    Item: {
-      id: uuid(),
-      text: data.text,
-      checked: false
-    }
-  };
+  console.log(event);
 
-  await client.put(params).promise();
+  switch (event.field) {
+    case 'createUser':
+        return await createUser(event.arguments.input)
+    case 'getUser':
+        return await getUsers(event.arguments.id)
+    case 'updateUser':
+        return await updateUsers(event.arguments.input)
+    case 'deleteUser':
+        return await deleteUsers(event.arguments.input.id)
+    default:
+      throw new Error(
+        'Invalid Request'
+      )
+  }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(params.Item)
-  };
+
+//   const data = JSON.parse(event.body);
+//  const result = await createItem(data);
+
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Headers': 'x-requested-with',
+//       'Access-Control-Allow-Credentials': true
+//   },
+//     body: JSON.stringify(result)
+//   };
 };
+
+
+const createUser = async (data) => {
+  const result = await createItem(data);
+  return result;
+}
+
+const createItem = (data) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "DemoTable",
+      Item: {
+        id: uuid(),
+        text: data.text,
+        checked: false
+      }
+    };
+  
+     client.put(params, (error, data) => {
+      if(error) {
+        reject(error)
+      }else {
+        resolve(params.Item)
+      }
+    })
+  })
+}
+
+
+const getUsers = async (data) => {
+  const result = await getItem(data);
+  return result;
+}
+
+const getItem = (data) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "DemoTable", 
+      Key: {
+        id: data,
+      }
+    };
+  
+    client.get(params, (error, data) => {
+      if(error) {
+        reject(error)
+      }else {
+        resolve(data.Item)
+      }
+    })
+  })
+}
+
+
+// <---- User Update ------>
+
+
+const updateUsers = async (data) => {
+  const result = await UpdateItem(data);
+  return result;
+}
+
+const UpdateItem = (data) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "DemoTable", 
+      Key: {
+        id: data.id,
+      },
+      UpdateExpression: 'set #t = :t',
+      ExpressionAttributeNames:{
+        '#t': 'text'
+      },
+      ExpressionAttributeValues: {
+        ':t' : data.text,
+      },
+      ReturnValues:"UPDATED_NEW"
+    };
+  
+    client.update(params, (error, data) => {
+      if(error) {
+        reject(error)
+      }else {
+        resolve(data.Attributes)
+      }
+    })
+  })
+}
+
+
+// <---- User Delete ------>
+
+
+const deleteUsers = async (data) => {
+  const result = await deleteItem(data);
+  return result;
+}
+
+const deleteItem = (data) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "DemoTable", 
+      Key: {
+        id: data,
+      }
+    };
+  
+    client.delete(params, (error, data) => {
+      if(error) {
+        reject(error)
+      }else {
+        resolve(data.Item)
+      }
+    })
+  })
+}
